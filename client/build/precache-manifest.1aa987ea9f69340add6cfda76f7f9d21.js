b43df01b0964c08d2748fb25001fed1d06e3f7a0
Dmitrii Zakharov <dzakharov@splunk.com>
2019-01-10 19:18:23 -0800

    after build

---
+++ client/build/precache-manifest.1aa987ea9f69340add6cfda76f7f9d21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fbe91524ee7fdc194d3e",
+    "url": "/static/js/main.fbe91524.chunk.js"
+  },
+  {
+    "revision": "70d5445ab58e6b834b25",
+    "url": "/static/js/1.70d5445a.chunk.js"
+  },
+  {
+    "revision": "fbe91524ee7fdc194d3e",
+    "url": "/static/css/main.e374ccef.chunk.css"
+  },
+  {
+    "revision": "9e1d15de565d1c0a6654871b8ce5390f",
+    "url": "/index.html"
+  }
+];
